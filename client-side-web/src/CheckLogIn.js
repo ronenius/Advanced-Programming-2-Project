4c517a6592d319ca152d5bcadad80b21eecb1912
ronenius <ronenius4@gmail.com>
2022-04-30 04:52:12 +0300

    login and register completed

--- client-side-web/src/CheckLogIn.js
+++ client-side-web/src/CheckLogIn.js
@@ -1,18 +1,22 @@
 import ConnectToMain from "./ConnectToMain.js";
+import users from "./DataBase.js";
 
 function CheckLogIn(username, labelUsername, password, labelPassword, validationText, navigate) {
-    ConnectToMain(username.current.value, password.current.value, navigate);
-    //ErrorLogIn(username, labelUsername, password, labelPassword, validationText);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username.current.value && users[i].password === password.current.value) {
+            ConnectToMain(username.current.value, password.current.value, navigate);
+            return;
+        }
+    }
+    ErrorLogIn(username, labelUsername, password, labelPassword, validationText);
 }
 
 function ErrorLogIn(username, labelUsername, password, labelPassword, validationText){
     labelUsername.current.style.color = "red";
-    username.current.value = "";
     username.current.style.boxShadow = "none";
     username.current.style.borderWidth = "2px";
     username.current.style.borderColor = "red";
     labelPassword.current.style.color = "red";
-    password.current.value = "";
     password.current.style.boxShadow = "none";
     password.current.style.borderWidth = "2px";
     password.current.style.borderTopWidth = "0px";
